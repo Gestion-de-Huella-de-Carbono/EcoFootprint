a942b7b8fc18677c2859b83258f6cca08c3261d3
Yeferson Salcedo <yeferson.salcedo@udea.edu.co>
2024-03-29 12:35:14 -0500

    agrego contenido al archivo carbonFootPrint.txt con las siguiente estructura: (kg C02eq/mes) transporte,energia,alimentos,otros

--- src/main/java/ApplicacionRunner.java
+++ src/main/java/ApplicacionRunner.java
@@ -1,2 +1,10 @@
+import java.text.ParseException;
+
 public class ApplicacionRunner {
+    public static void main(String[] args) throws ParseException {
+
+        //PersonService personService = new PersonService(new PersonInMemory());
+        //PersonService personService = new PersonService(new PersonFileBased());
+
+    }
 }

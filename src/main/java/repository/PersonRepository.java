065d9ad1874149560ba61ad606c4323464e361d3
JuanMauwu <juanmavc111@gmail.com>
2024-03-29 16:25:44 -0500

    se agrega contenido a la interfaz PersonRepository

--- src/main/java/repository/PersonRepository.java
+++ src/main/java/repository/PersonRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import data.Person;
+
 public interface PersonRepository {
+    List <Person> findAll();
+    default Optional<Person> save(Person person) {
+        return null;
+    }
 }

34db27e7619b723a2581fa3a59d345bb0663249d
JuanEstebanObando <juanesteban.obando@udea.edu.co>
2024-03-31 11:05:34 -0500

    Añade constructor, getter y setter en la clase persona

--- src/main/java/data/Person.java
+++ src/main/java/data/Person.java
@@ -1,4 +1,54 @@
 package data;
-
-public record Person() {
+public class Person {
+	private String name;
+	private String lastName;
+	private int age;
+	private String city;
+	private double totalCarbonFootprint;
+	
+	//Inicio del constructor
+	public Person(String name,String lastName, int age, String city, double totalCarbonFootprint) {
+		this.name = name;
+		this.lastName = lastName;
+		this.age = age;
+		this.city = city;
+		this.totalCarbonFootprint = totalCarbonFootprint;
+	}
+	
+	//Getters
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getCity() {
+		return city;
+	}
+	public double getTotalCarbonFootprint() {
+		return totalCarbonFootprint;
+	}
+	
+	//Setters
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setAge(int age) {
+	    this.age = age;
+	}
+	public void setCity(String city) {
+        this.city = city;
+    }
+	public void setTotalCarbonFootprint(double totalCarbonFootprint) {
+        this.totalCarbonFootprint = totalCarbonFootprint;
+    }
 }

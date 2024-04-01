bdfef687f3818a79d316711f285c1cc1f2517b8f
Juan Cadena <juanm.cadena@udea.edu.co>
2024-04-01 18:49:34 -0500

    agrego  contenido CarbonFootprint

--- src/main/java/data/CarbonFootprint.java
+++ src/main/java/data/CarbonFootprint.java
@@ -1,4 +1,49 @@
 package data;
 
 public class CarbonFootprint {
+private Float transport;
+private Float energy;
+private Float food;
+private Float other;
+
+
+    public CarbonFootprint(Float transport, Float energy, Float food, Float other) {
+        this.transport = transport;
+        this.energy = energy;
+        this.food = food;
+        this.other = other;
+
+    }
+
+    public Float getTransport() {
+        return transport;
+    }
+
+    public Float getEnergy() {
+        return energy;
+    }
+
+    public Float getFood() {
+        return food;
+    }
+
+    public Float Other() {
+        return other;
+    }
+
+    public void setTransport(Float transport) {
+        this.transport = transport;
+    }
+
+    public void setEnergy(Float energy) {
+         this.energy = energy;
+    }
+
+    public void setFood(Float food) {
+        this.food = food;
+    }
+
+    public void setOther(Float other) {
+        this.other = other;
+    }
 }
--- src/main/java/repository/Impl/PersonInMemory.java
+++ src/main/java/repository/Impl/PersonInMemory.java
@@ -1,4 +1,20 @@
 package repository.Impl;
 
-public class PersonInMemory {
+import data.Person;
+import repository.PersonRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonInMemory implements PersonRepository {
+
+    private List<Person> persons;
+
+    public PersonInMemory() {
+        this.customers = new ArrayList<>();
+
+    @Override
+    public List<Person> findAll() {
+        return null;
+    }
 }

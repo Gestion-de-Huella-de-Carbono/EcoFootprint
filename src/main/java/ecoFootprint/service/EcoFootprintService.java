72872557bfd49aaf04ec4288110fa5715d2f1568
JuanEstebanObando <juanesteban.obando@udea.edu.co>
2024-05-26 13:01:13 -0500

    Add metods to use in the interface, and add functions in the implement.

--- src/main/java/ecoFootprint/service/EcoFootprintService.java
+++ src/main/java/ecoFootprint/service/EcoFootprintService.java
@@ -1,4 +1,11 @@
 package ecoFootprint.service;
 
+import java.util.List;
+import ecoFootprint.model.Person;
+import ecoFootprint.model.CarbonFootprint;
+
 public interface EcoFootprintService {
+	List<Person> findAllPersons();
+	List<CarbonFootprint> findAllCarbonFootprintData();
+	List<Person> showSameAgePeople(int age);
 }

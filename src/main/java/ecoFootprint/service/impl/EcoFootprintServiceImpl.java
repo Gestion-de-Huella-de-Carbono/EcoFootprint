2921a242dc63d982d0bfa461d7ade84b00216c18
JuanEstebanObando <juanesteban.obando@udea.edu.co>
2024-05-26 13:01:13 -0500

    Add metods to use in the interface, and add functions in the implement.

--- src/main/java/ecoFootprint/service/impl/EcoFootprintServiceImpl.java
+++ src/main/java/ecoFootprint/service/impl/EcoFootprintServiceImpl.java
@@ -1,4 +1,33 @@
 package ecoFootprint.service.impl;
+import java.util.List;
+import ecoFootprint.model.Person;
+import ecoFootprint.model.CarbonFootprint;
+import ecoFootprint.repository.PersonRepository;
+import ecoFootprint.repository.CarbonFootprintRepository;
+import ecoFootprint.service.EcoFootprintService;
 
-public class EcoFootprintServiceImpl {
+public class EcoFootprintServiceImpl implements EcoFootprintService{
+	
+	private final PersonRepository personRepository;
+    private final CarbonFootprintRepository carbonFootprintRepository;
+
+    public EcoFootprintServiceImpl(PersonRepository personRepository, CarbonFootprintRepository carbonFootprintRepository) {
+        this.personRepository = personRepository;
+        this.carbonFootprintRepository = carbonFootprintRepository;
+    }
+
+
+    @Override
+	public List<Person> findAllPersons() {
+		return personRepository.findAll();		
+	}
+	@Override
+	public List<CarbonFootprint> findAllCarbonFootprintData() {
+		return carbonFootprintRepository.findAll();
+	}
+	@Override
+	public List<Person> showSameAgePeople(int age) {
+		List<Person> allPersons = personRepository.findAll();
+		return personRepository.showSameAgePeople(allPersons, age);
+	}	
 }
